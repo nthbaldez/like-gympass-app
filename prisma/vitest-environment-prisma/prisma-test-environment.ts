85295dc32f26110d987af323279d793a2b256e44
Nathan Baldez <nathanbsb2@hotmail.com>
2025-04-08 16:07:48 -0300

    tests: config tests environments, criação de schemas dinâmicos para cada rodada de tests

--- prisma/vitest-environment-prisma/prisma-test-environment.ts
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -1,13 +1,37 @@
+import 'dotenv/config'
+import { randomUUID } from 'node:crypto'
+import { execSync } from 'node:child_process'
 import type { Environment } from 'vitest/environments'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+function generateDatabaseURL(schema: string) {
+  if (!process.env.DATABASE_URL) {
+    throw new Error('Please provide a DATABASE_URL environment variable.')
+  }
+
+  const url = new URL(process.env.DATABASE_URL)
+  url.searchParams.set('schema', schema)
+  return url.toString()
+}
 
 export default <Environment>{
   name: 'prisma',
   transformMode: 'web',
   async setup() {
-    console.log('Executou setup!')
+    const schema = randomUUID()
+    const DATABASE_URL = generateDatabaseURL(schema)
+
+    process.env.DATABASE_URL = DATABASE_URL
+
+    execSync('npx prisma migrate deploy')
+
     return {
-      teardown() {
-        console.log('Finalizou através do teardown')
+      async teardown() {
+        await prisma.$executeRawUnsafe(
+          `DROP SCHEMA IF EXISTS "${schema}" CASCADE`,
+        )
       },
     }
   },
--- vite.config.mts
+++ vite.config.mts
@@ -3,24 +3,4 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
   plugins: [tsconfigPaths()],
-  test: {
-    workspace: [
-      {
-        extends: true,
-        test: {
-          include: ['src/http/controllers/*.spec.ts'],
-          environment:
-            'prisma/vitest-environment-prisma/prisma-test-environment.ts',
-        },
-      },
-      {
-        extends: true,
-        test: {
-          include: ['src/use-cases/*.spec.{ts,js}'],
-          name: 'node',
-          environment: 'node',
-        },
-      },
-    ],
-  },
 })
